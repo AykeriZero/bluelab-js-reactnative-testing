4434730ffd0fd7c656eee25b49c2407e7cdaed76
songya <songya@umich.edu>
2018-08-28 21:18:18 -0500

    formatted view tag properties and added props to index.js

--- albums/index.js
+++ albums/index.js
@@ -1,13 +1,11 @@
 // Import a library to help create a component
-
 import React from 'react';
 import { AppRegistry } from 'react-native';
 import Header from './src/components/header';
 
 // Create a component
-
 const App = () => (
-    <Header />
+    <Header headerText={'Albums'} />
 );
 
 
