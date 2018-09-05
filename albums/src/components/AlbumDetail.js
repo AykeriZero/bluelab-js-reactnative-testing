64bbe91c44483e353f2787a24980625c34111c4c
songya <songya@umich.edu>
2018-09-05 12:04:09 -0400

    added buttons

--- albums/src/components/AlbumDetail.js
+++ albums/src/components/AlbumDetail.js
@@ -5,16 +5,24 @@ import React from 'react';
 import { View, Text, Image } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
+import Button from './Button';
 
 const AlbumDetail = ({ album }) => {
-  const { title, artist, thumbnail_image } = album;
-  const { thumbnailStyle, headerTextStyle } = styles;
+  const { title, artist, thumbnail_image, image } = album;
+  const {
+    thumbnailStyle,
+    headerTextStyle,
+    headerTitleStyle,
+    thumbnailContainterStyle,
+    imageStyle
+  } = styles;
 
   return (
     <Card>
+
       <CardSection>
 
-        <View>
+        <View style={thumbnailContainterStyle}>
           <Image
             source={{ uri: thumbnail_image }}
             style={thumbnailStyle}
@@ -22,11 +30,23 @@ const AlbumDetail = ({ album }) => {
         </View>
 
         <View style={headerTextStyle} >
-          <Text>{title}</Text>
+          <Text style={headerTitleStyle}>{title}</Text>
           <Text>{artist}</Text>
         </View>
 
       </CardSection>
+
+      <CardSection>
+        <Image
+          source={{ uri: image }}
+          style={imageStyle}
+        />
+      </CardSection>
+
+      <CardSection>
+        <Button />
+      </CardSection>
+
     </Card>
   );
 };
@@ -38,11 +58,26 @@ const styles = {
     justifyContent: 'space-around'
   },
 
+  headerTitleStyle: {
+    fontSize: 18
+  },
+
   thumbnailStyle: {
     height: 50,
     width: 50
-  }
+  },
 
+  thumbnailContainterStyle: {
+    justifyContent: 'center',
+  //  marginLeft: 10,
+    marginRight: 10
+  },
+
+  imageStyle: {
+    height: 350,
+    flex: 1,
+    width: null
+  }
 };
 
 export default AlbumDetail;
--- albums/src/components/Card.js
+++ albums/src/components/Card.js
@@ -22,7 +22,8 @@ const styles = {
     elevation: 1,
     marginLeft: 5,
     marginRight: 5,
-    marginTop: 10
+    marginTop: 5,
+    marginBottom: 5
   }
 };
 
