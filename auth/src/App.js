a76e78c477fc08bc7f24c48c60ca5112dba08e17
songya <songya@umich.edu>
2018-09-09 16:08:35 -0400

    added firebase packages for authentication

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
+import firebase from 'firebase';
+
 import { Header } from './components/common';
+import config from './config.js';
 
 class App extends Component {
   componentWillMount() {
-
+    firebase.initializeApp(config);
   }
 
   render() {
