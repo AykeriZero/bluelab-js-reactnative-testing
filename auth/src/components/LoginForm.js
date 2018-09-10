541f2080f179afa3081888e5afc4598ab1885faf
songya <songya@umich.edu>
2018-09-09 22:58:02 -0400

    Input component and email and password fields to LoginForm

--- auth/src/components/LoginForm.js
+++ auth/src/components/LoginForm.js
@@ -1,1 +1,37 @@
 import React, { Component } from 'react';
+import { Card, CardSection, Button, Input } from './common';
+
+class LoginForm extends Component {
+  state = { email: '', password: '' };
+
+  render() {
+    return (
+      <Card>
+        <CardSection>
+          <Input
+            placeholder="user@email.com"
+            label="Email"
+            value={this.state.email}
+            onChangeText={email => this.setState({ email })}
+          />
+        </CardSection>
+
+        <CardSection>
+          <Input
+            placeholder="iLoveCats123"
+            label="Password"
+            value={this.state.password}
+            onChangeText={password => this.setState({ password })}
+          />
+        </CardSection>
+
+        <CardSection>
+          <Button>Log in</Button>
+        </CardSection>
+      </Card>
+    );
+  }
+}
+
+
+export default LoginForm;
