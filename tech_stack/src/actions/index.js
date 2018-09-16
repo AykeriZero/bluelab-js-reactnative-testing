0486a91704d55e84d605b9e57261d5f9aba4b676
songya <songya@umich.edu>
2018-09-16 15:52:03 -0400

    added action creators to listitem component

--- tech_stack/src/actions/index.js
+++ tech_stack/src/actions/index.js
@@ -1,7 +1,7 @@
 
 // create Action Creator SelectedLibrary which returns an action with the
 // payload of a new LibraryId
-export const SelectedLibrary = (libraryId) => ({
+export const SelectLibrary = (libraryId) => ({
     type: 'select_Library',
     payload: libraryId
 });
